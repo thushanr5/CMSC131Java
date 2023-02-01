c7002c95c2a441cc9e3439927218eb1bd05e6397
Thushan <88255435+thushanr5@users.noreply.github.com>
2023-02-01 09:13:27 -0500

    Create Earth.java

---
+++ OOP/Earth.java
@@ -0,0 +1,16 @@
+//Earth class Java
+
+public class Earth {
+	
+	public static void main(String[]args) {
+		Human Tom = new Human();
+		
+		Tom.age= 5;
+		Tom.eyeColor = "brown";
+		Tom.heightInInches = 25;
+		Tom.name = "Tom";
+		Tom.speak();
+	
+		
+	}
+}
