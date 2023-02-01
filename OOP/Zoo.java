02e52348fd5281efb0e308f2bc880afbe201409f
Thushan <88255435+thushanr5@users.noreply.github.com>
2023-02-01 09:53:29 -0500

    Create Zoo.java

---
+++ OOP/Zoo.java
@@ -0,0 +1,11 @@
+//Calling of the Animal class
+
+public class Zoo {
+
+	public static void main(String[] args) {
+		Animal lion = new Animal("black",2,"yellow",2,"roar");
+		lion.speak();
+		lion.teethColorMove();
+	}
+
+}
