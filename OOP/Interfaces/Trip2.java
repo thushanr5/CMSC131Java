013de96075a046264644e11be8f1591cb25b5cd9
Thushan <88255435+thushanr5@users.noreply.github.com>
2023-02-02 17:56:49 -0500

    Create Trip2.java

---
+++ OOP/Interfaces/Trip2.java
@@ -0,0 +1,14 @@
+public class Trip2 extends Guide implements Meal{
+
+    public Trip2(String nameOfPerson, String currency, int age, int moneyLeftDollars){
+        super(nameOfPerson,currency,age,moneyLeftDollars);
+    }
+
+    @Override
+    public void eat(){
+        System.out.println("The dinner " + this.nameOfPerson + " had was rolls");
+    }
+
+
+
+}
