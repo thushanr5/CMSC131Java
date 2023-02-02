61799b7dcb86bc561f869bcfb5181eb26bc58a7d
Thushan <88255435+thushanr5@users.noreply.github.com>
2023-02-02 17:56:30 -0500

    Create Guide.java

---
+++ OOP/Interfaces/Guide.java
@@ -0,0 +1,21 @@
+public class Guide extends Trip implements Meal{
+    int relaxLevel;
+    String food;
+    public Guide(String nameOfPerson, String currency, int age, int moneyLeftDollars){
+        super(nameOfPerson,currency,age,moneyLeftDollars);
+    }
+    public void travelTips(){
+        System.out.println("The tips for enjoying your stay in Sri Lanka include " + this.nameOfPerson);
+    }
+
+    public double currencyConvert(){
+        return (0.012 * this.moneyLeftDollars);
+    }
+
+
+    public void eat(){
+        System.out.println("The dinner " + this.nameOfPerson + " had was Hoppers");
+    }
+
+
+}
