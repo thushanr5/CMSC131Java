e1ad337a4ce59b29a1e404c0462ed46e88a6a243
Thushan <88255435+thushanr5@users.noreply.github.com>
2023-02-02 14:44:55 -0500

    Create DC.java

---
+++ OOP/Inheritance/DC.java
@@ -0,0 +1,10 @@
+//DC is the child class and the parent class is MarvelVerse: Aka super class and subclass
+public class DC extends MarvelVerse{
+
+    public DC(String nameOfCharacter, String power, char flyable, int health, int damage, int speed){
+        super(nameOfCharacter,power,flyable,flyable,damage,speed);
+
+    }
+
+
+}
