cbdcd1f02a121ca3de1acf2158584969ad106c6a
Thushan <88255435+thushanr5@users.noreply.github.com>
2023-02-01 23:49:10 -0500

    Update Zoo.java

--- OOP/Practice2/Zoo.java
+++ OOP/Practice2/Zoo.java
@@ -1,11 +1,17 @@
-//Calling of the Animal class
+package LearningFiles;
 
 public class Zoo {
-
+	
+	//Calling of the Animal class
 	public static void main(String[] args) {
 		Animal lion = new Animal("black",2,"yellow",2,"roar");
 		lion.speak();
 		lion.teethColorMove();
+			
+		Bird bird1 = new Bird("black",2,"yellow",2,"roar");
+		bird1.fly(); //Accesible methods include methods in Bird class + Animal class because the bird is the child class of the super class Animal
+		
 	}
 
 }
+
