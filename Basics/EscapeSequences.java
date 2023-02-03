2f3a43670600132ddb32861c6204e903aec9cbdc
Thushan <88255435+thushanr5@users.noreply.github.com>
2023-02-03 16:42:41 -0500

    Create EscapeSequences.java

---
+++ Basics/EscapeSequences.java
@@ -0,0 +1,6 @@
+/*
+System.out.println("Good Morning\t Geeks! "); ->Tab
+System.out.println("Good Morning Geeks! \n How are you all?  "); ->New line
+System.out.println("Good Morning \'Geeks!\' How are you all?  "); ->Single Quote
+System.out.println("\\- this is a backslash. "); ->Single Slash
+*/
