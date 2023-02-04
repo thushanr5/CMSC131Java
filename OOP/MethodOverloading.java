b8f544adb3a4d1497875ee3fc7eab382dfa4ae3c
Thushan Ranasinghe <88255435+thushanr5@users.noreply.github.com>
2023-02-04 12:24:59 -0500

    Create MethodOverloading.java

---
+++ OOP/MethodOverloading.java
@@ -0,0 +1,25 @@
+public class FirstClass {
+  
+    public static void main(String[]args){
+    
+    }
+  
+    public static void doSomething(int a){
+
+    }
+    public static int doSomething(int a){
+
+    }
+  
+  //Causes an error because same name and same parameter
+  
+    public static void doSomething(int a){
+
+    }
+    public static int doSomething(int a){
+
+    }
+  
+  //Returns types and visibility modifier doesn't matter when comparing two methods
+  
+}
