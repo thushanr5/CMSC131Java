125337698aee12a5f9701fefbb649d7e9beac9c2
Thushan Ranasinghe <88255435+thushanr5@users.noreply.github.com>
2023-02-05 23:04:07 -0500

    Update Switch.java

--- Rarely used implementations/Switch.java
+++ Rarely used implementations/Switch.java
@@ -29,4 +29,23 @@ swtich(fruit){
     break;
 }
 
+//Example
+
+int switchValue = 4;
+
+switch(switchValue){
+  case 1: 
+     System.out.println("The value was 1");
+     break;
+  case 2:
+    System.out.println("The value was 2");
+    break;
+  case 3: case 4: case 5: 
+    System.out.println("The value was " + switchValue);
+    break;
+  default:
+    System.out.println("The value was neither a 1,2,3,4,5");
+    break;
+}
+
   
