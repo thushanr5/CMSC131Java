3dae1f2cd752829f5d17c1149d443fbe8a062766
Thushan Ranasinghe <88255435+thushanr5@users.noreply.github.com>
2023-02-05 22:59:32 -0500

    Update and rename Switch.txt to Switch.java

---
+++ Rarely used implementations/Switch.java
@@ -0,0 +1,32 @@
+// Variable can be tested for equality among different values
+
+// syntax:
+
+switch (variable){
+  case 1:
+  System.out.println("case 1");
+  break;
+  
+  case 2:
+  System.out.println("case 2");
+  break;
+  
+}
+
+//Example
+
+String fruit = "watermelon";
+
+swtich(fruit){
+  case "apple":
+    System.out.println("The fruit was a watermelon");
+    break;
+  case "peach":
+    System.out.println("The fruit was a peach");
+    break;
+  case "watermelon":
+    System.out.println("The fruit was a watermelon");
+    break;
+}
+
+  
