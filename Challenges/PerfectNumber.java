aa53772e3bbae88c20dc19795e178ce048e3b94f
Thushan Ranasinghe <88255435+thushanr5@users.noreply.github.com>
2023-02-13 15:02:10 -0500

    Create PerfectNumber.java

---
+++ Challenges/PerfectNumber.java
@@ -0,0 +1,13 @@
+public class PerfectNumber {
+    public static boolean isPerfectNumber(int number){
+        int sum = 0;
+        if(number >= 0){
+            for(int num = number; num>=0; num--){
+                if(number / num ==0 && num !=number)
+                    sum+=num;
+        }
+        return sum == number ? true : false;
+    }
+    return false;
+}
+}
