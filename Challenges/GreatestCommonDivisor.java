078ab51927c97cfdc33f25d807f8968a40209e48
Thushan Ranasinghe <88255435+thushanr5@users.noreply.github.com>
2023-02-13 18:49:33 -0500

    Create GreatestCommonDivisor.java

---
+++ Challenges/GreatestCommonDivisor.java
@@ -0,0 +1,17 @@
+public class GreatestCommonDivisor {
+    public static int getGreatestCommonDivisor(int first, int second){
+        int maxNum = 0;
+        if(first >=10 && second >=10){
+            for(int firstNum = first; firstNum >=0; firstNum--){
+               for(int secondNum = second; secondNum >=0; secondNum--){
+                   if(first % firstNum == 0 && second % secondNum ==0)
+                        if(firstNum > maxNum)
+                            maxNum = firstNum;
+                        break;
+               }
+            }
+            return maxNum;
+        }
+        return -1;
+    }
+}
